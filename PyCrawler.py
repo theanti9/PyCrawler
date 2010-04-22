4accdac7a8f484550c224861170be4d0f304529d
Ryan Merl <the.anti.9@gmail.com>
2010-04-21 22:18:26 -0400

    removed threading as it produced no performance gain and also was bad in combination with SQLite according to their documentation.

--- PyCrawler.py
+++ PyCrawler.py
@@ -8,7 +8,7 @@ try:
 	import psyco
 	psyco.full()
 except ImportError:
-	pass
+	print "Continuing without psyco JIT compilation!"
 
 connection = sqlite.connect('crawl.db')
 cursor = connection.cursor()
@@ -22,57 +22,63 @@ except:
 	pass
 keywordregex = re.compile('<meta\sname=["\']keywords["\']\scontent=["\'](.*?)["\']\s/>')
 linkregex = re.compile('<a\s*href=[\'|"](.*?)[\'"].*?>')
+crawled = []
 class threader ( threading.Thread ):
 	def run(self):
 		while 1:
-			try:
-				cursor.execute("SELECT * FROM queue")
-				crawling = cursor.fetchone()
-				crawling = crawling[0]
-				cursor.execute("DELETE FROM queue WHERE url = (?)", (crawling, ))
-				connection.commit()
-				print crawling
-			except KeyError:
-				raise StopIteration
-			url = urlparse.urlparse(crawling)
-			try:
-				response = urllib2.urlopen(crawling)
-			except:
-				continue
-			msg = response.read()
-			startPos = msg.find('<title>')
-			if startPos != -1:
-				endPos = msg.find('</title>', startPos+7)
-				if endPos != -1:
-					title = msg[startPos+7:endPos]
-			keywordlist = keywordregex.findall(msg)
-			if len(keywordlist) > 0:
-				keywordlist = keywordlist[0]
-			else:
-				keywordlist = ""
-			links = linkregex.findall(msg)
-			title.replace("'", "\'")
-			keywordlist.replace("'", "\'")
-			cursor.execute("SELECT url FROM crawl_index")
-			crawled = cursor.fetchall()
-			for link in (links.pop(0) for _ in xrange(len(links))):
-				if link.startswith('/'):
-					link = 'http://' + url[1] + link
-				elif link.startswith('#'):
-					link = 'http://' + url[1] + url[2] + link
-				elif not link.startswith('http'):
-					link = 'http://' + url[1] + '/' + link
-				if link.decode('utf-8') not in crawled:
-					try:
-						cursor.execute("INSERT INTO queue VALUES ( (?) )", (link, ))
-						connection.commit()
-					except:
-						continue
-			try:
-				cursor.execute("INSERT INTO crawl_index VALUES( (?), (?), (?) )", (crawling, title, keywordlist))
-				connection.commit()
-			except:
-				continue
+			crawl()
+			
+	def crawl():
+		try:
+			cursor.execute("SELECT * FROM queue LIMIT 1")
+			crawling = cursor.fetchone()
+			crawling = crawling[0]
+			cursor.execute("DELETE FROM queue WHERE url = (?)", (crawling, ))
+			connection.commit()
+			print crawling
+		except KeyError:
+			raise StopIteration
+		url = urlparse.urlparse(crawling)
+		try:
+			crawled.append(crawling)
+		except MemoryError:
+			del crawled[:]
+		try:
+			response = urllib2.urlopen(crawling)
+		except:
+			continue
+		msg = response.read()
+		startPos = msg.find('<title>')
+		if startPos != -1:
+			endPos = msg.find('</title>', startPos+7)
+			if endPos != -1:
+				title = msg[startPos+7:endPos]
+		keywordlist = keywordregex.findall(msg)
+		if len(keywordlist) > 0:
+			keywordlist = keywordlist[0]
+		else:
+			keywordlist = ""
+		links = linkregex.findall(msg)
+		title.replace("'", "\'")
+		keywordlist.replace("'", "\'")
 
-for i in range(10):
+		for link in (links.pop(0) for _ in xrange(len(links))):
+			if link.startswith('/'):
+				link = 'http://' + url[1] + link
+			elif link.startswith('#'):
+				link = 'http://' + url[1] + url[2] + link
+			elif not link.startswith('http'):
+				link = 'http://' + url[1] + '/' + link
+			if link.decode('utf-8') not in crawled:
+				try:
+					cursor.execute("INSERT INTO queue VALUES ( (?) )", (link, ))
+					connection.commit()
+				except:
+					continue
+		try:
+			cursor.execute("INSERT INTO crawl_index VALUES( (?), (?), (?) )", (crawling, title, keywordlist))
+			connection.commit()
+		except:
+			continue
+if __name__ == '__main__':
 	threader().run()
